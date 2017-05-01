93c532ef93d1fcec5bd4f809904612dfe85939da
Grady Shelton and Niklas Long <grady@>
2017-05-01 16:38:35 -0700

    add sinatra routing for homepage and js

--- app.rb
+++ app.rb
@@ -1,12 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/task'
+require './lib/list'
+require 'pg'
 require 'pry'
 
 DB = PG.connect({:dbname => "to_do"})
 
 also_reload('lib/**/*.rb')
 
-get('/') do
+get("/") do
+  @list = List.new({:name => "Stuff", :id => 1})
+  @tasks = Task.all()
   erb(:index)
+end
+
+get "/tasks" do
+  erb(:index)
+end
+
+post("/tasks/new") do
+  description = params.fetch("description")
+  due_date = params.fetch("due_date")
+  list_id = 1
+  task = Task.new({:description => description, :due_date => due_date, :list_id => list_id})
+  task.save()
+  erb(:success)
 end
---
+++ public/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $("#new-task").click(function(){
+    $(".new-task").show();
+  });
+});
